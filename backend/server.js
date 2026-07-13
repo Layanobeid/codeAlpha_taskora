9085ed1e9a13ac91f92076b02c2becdca00853fa
Layanobeid <86520396+Layanobeid@users.noreply.github.com>
2026-07-13 18:19:46 +0300

    Add notification routes to server

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const authRoutes = require('./src/routes/authRoutes');
 const projectRoutes = require('./src/routes/projectRoutes');
 const taskRoutes = require('./src/routes/taskRoutes');
 const aiRoutes = require('./src/routes/aiRoutes');
+const notificationRoutes = require('./src/routes/notificationRoutes');
 
 // Import middleware
 const { errorHandler } = require('./src/middleware/errorHandler');
@@ -46,7 +47,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/ai', aiRoutes);
-
+app.use('/api/notifications', notificationRoutes);
 // Home route
 app.get('/', (req, res) => {
   res.json({
